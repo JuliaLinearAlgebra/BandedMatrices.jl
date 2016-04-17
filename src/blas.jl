1b9d481581619b6c587ca8940d2e859e9eda6c25
Sheehan Olver <solver@mac.com>
2016-04-18 09:21:29 +1000

    restrict columns of A

--- src/blas.jl
+++ src/blas.jl
@@ -51,10 +51,10 @@ function gbmm!{T}(alpha,A::BandedMatrix{T},B::BandedMatrix{T},beta,C::BandedMatr
 
     #Multiply columns where index of B starts from 1
     for j=1:min(B.u+1,m)
-        gbmv!('N',min(C.l+j,n), A.l, A.u, alpha, a, B.l+j, sta, b+sz*((j-1)*stb+B.u-j+1), beta, c+sz*((j-1)*stc+C.u-j+1))
+        gbmv!('N',min(C.l+j,n), A.l, A.u, alpha, a, min(B.l+j,ν), sta, b+sz*((j-1)*stb+B.u-j+1), beta, c+sz*((j-1)*stc+C.u-j+1))
     end
     #Multiply columns where index of C starts from 1
-    for j=B.u+2:min(C.u+1,m)
+    for j=B.u+2:min(C.u+1,m,ν+B.u)
         p=j-B.u-1
         gbmv!('N', min(C.l+j,n), A.l+p, A.u-p, alpha, a+sz*p*sta, B.l+B.u+1, sta, b+sz*(j-1)*stb, beta, c+sz*((j-1)*stc+C.u-j+1))
     end
@@ -65,12 +65,12 @@ function gbmm!{T}(alpha,A::BandedMatrix{T},B::BandedMatrix{T},beta,C::BandedMatr
     end
 
     # multiply columns where A and B are mid and C is bottom
-    for j=max(n-C.l+1,1):min(ν-B.l,n+C.u,m)
+    for j=max(n-C.l+1,C.u+1):min(ν-B.l,n+C.u,m)
         gbmv!('N', n-j+C.u+1, A.l+A.u, 0, alpha, a+sz*(j-B.u-1)*sta, B.l+B.u+1, sta, b+sz*(j-1)*stb, beta, c+sz*(j-1)*stc)
     end
 
     # multiply columns where A,  B and C are bottom
-    for j=max(ν-B.l+1,1):min(m,n+C.u)
+    for j=max(ν-B.l+1,C.u+1):min(m,n+C.u)
         gbmv!('N', n-j+C.u+1, A.l+A.u, 0, alpha, a+sz*(j-B.u-1)*sta, B.l+B.u+1-(j-ν+B.l), sta, b+sz*(j-1)*stb, beta, c+sz*(j-1)*stc)
     end
 
