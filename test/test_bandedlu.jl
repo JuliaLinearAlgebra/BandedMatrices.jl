040938ba68d8a73bc8577af98b6d1391f0c5e812
Pablo Zubieta <pabloferz@yahoo.com.mx>
2016-08-01 19:17:30 +0200

    Remove special test for master

--- test/test_bandedlu.jl
+++ test/test_bandedlu.jl
@@ -24,11 +24,7 @@ let
     @test BandedMatrices._promote_to_blas_type(typ, Int16)      == typ
     @test BandedMatrices._promote_to_blas_type(typ, Int8)       == typ
 
-    if VERSION < v"0.5.0-dev+5122"
-        @test_throws ErrorException BandedMatrices._promote_to_blas_type(_foo, Float64)
-    else
-        @test_throws MethodError BandedMatrices._promote_to_blas_type(_foo, Float64)
-    end
+    @test_throws ErrorException BandedMatrices._promote_to_blas_type(_foo, Float64)
 end
 
 # conversion of inputs to appropriate blas type
