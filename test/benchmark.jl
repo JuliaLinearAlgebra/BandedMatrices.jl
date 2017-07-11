91ce474ed2375d71f52a39d83d39581e1be93c83
Randy Lai <randy.cs.lai@gmail.com>
2017-07-10 22:46:30 -0400

    update benchmark code

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -62,6 +62,52 @@ end
 
 #gc_enable(true)
 
+function julia(n, b1, b2)
+    A = brand(n, n, b1, b2)
+    BandedMatrices.banded_generic_matmatmul!(BandedMatrix(Float64,n, n, 2*b1, 2*b2), A, A)
+end
+
+function blas(n, b1, b2)
+    A = brand(n, n, b1, b2)
+    BandedMatrices.gbmm!(one(Float64), A, A, zero(Float64), BandedMatrix(Float64,n, n, 2*b1, 2*b2))
+end
+
+function julia(n, b1, b2)
+    A = brand(n, n, b1, b2)
+    D = rand(n, 2)
+    BandedMatrices.banded_generic_matmatmul!(Matrix{Float64}(n, 2), A, D)
+end
+
+function blas(n, b1, b2)
+    A = brand(n, n, b1, b2)
+    D = rand(n, 2)
+    BandedMatrices.gbmm!(one(Float64), A, D, zero(Float64), Matrix{Float64}(n, 2))
+end
+
+@time julia(100, 1, 1);
+@time blas(100, 1, 1);
+@time julia(100, 50, 50);
+@time blas(100, 50, 50);
+@time julia(100, 75, 75);
+@time blas(100, 75, 75);
+@time julia(1000, 2, 3);
+@time blas(1000, 2, 3);
+@time julia(1000, 50, 50);
+@time blas(1000, 50, 50);
+@time julia(1000, 200, 300);
+@time blas(1000, 200, 300);
+@time julia(10000, 2, 3);
+@time blas(10000, 2, 3);
+@time julia(10000, 50, 50);
+@time blas(10000, 50, 50);
+@time julia(10000, 200, 300);
+@time blas(10000, 200, 300);
+@time julia(100000, 2, 3);
+@time blas(100000, 2, 3);
+@time julia(100000, 50, 50);
+@time blas(100000, 50, 50);
+@time julia(100000, 200, 300);
+@time blas(100000, 200, 300);
 
 
 # println("Large dimension QR solve")
@@ -81,4 +127,4 @@ end
 
 
 # println("Time should be   0.401671 seconds (24 allocations: 36.665 MB)")
-# println("Time should be   0.009276 seconds (30 allocations: 12.262 MB)")
+# println("Time should be   0.009276 seconds (30 allocations: 12.262 MB
