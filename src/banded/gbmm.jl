ca26051e19830a00dc402e8601ac858dee418f0f
Sheehan Olver <dlfivefifty@gmail.com>
2018-02-19 14:09:24 +0000

    scalemul! -> mul!

--- src/banded/gbmm.jl
+++ src/banded/gbmm.jl
@@ -317,18 +317,18 @@ function gbmm!(tA::Char, tB::Char, α::T, A::StridedMatrix{T}, B::AbstractMatrix
     C
 end
 
-scalemul!(α, A, x, β, y) = _scalemul!(α, A, x, β, y, memorylayout(A), memorylayout(x), memorylayout(y))
-_scalemul!(α, A, x, β, y, blasA, blasx, blasy) = (y .= α .* A*x .+ β.*y)
-_scalemul!(α, A::AbstractMatrix{T}, x::AbstractVector{T}, β, y::AbstractVector{T},
+mul!(α, A, x, β, y) = _mul!(α, A, x, β, y, memorylayout(A), memorylayout(x), memorylayout(y))
+_mul!(α, A, x, β, y, blasA, blasx, blasy) = (y .= α .* A*x .+ β.*y)
+_mul!(α, A::AbstractMatrix{T}, x::AbstractVector{T}, β, y::AbstractVector{T},
                    ::StridedLayout{T}, ::BandedLayout{T}, ::StridedLayout{T}) where {T<:BlasFloat} =
     gbmv!('N', α, A, x, β, y)
-_scalemul!(α, A::AbstractMatrix{T}, x::AbstractVector{T}, β, y::AbstractVector{T},
+_mul!(α, A::AbstractMatrix{T}, x::AbstractVector{T}, β, y::AbstractVector{T},
                    ::StridedLayout{T}, ::StridedLayout{T}, ::StridedLayout{T}) where {T<:BlasFloat} =
     BLAS.gemv!('N', α, A, x, β, y)
 
-_scalemul!(α, A::AbstractMatrix{T}, B::AbstractMatrix{T}, β, C::AbstractMatrix{T},
+_mul!(α, A::AbstractMatrix{T}, B::AbstractMatrix{T}, β, C::AbstractMatrix{T},
                    ::BandedLayout{T}, ::BandedLayout{T}, ::BandedLayout{T}) where {T<:BlasFloat} =
     gbmm!('N', 'N', α, A, B, β, C)
-_scalemul!(α, A::AbstractMatrix{T}, B::AbstractMatrix{T}, β, C::AbstractMatrix{T},
+_mul!(α, A::AbstractMatrix{T}, B::AbstractMatrix{T}, β, C::AbstractMatrix{T},
                    ::StridedLayout{T}, ::StridedLayout{T}, ::StridedLayout{T}) where {T<:BlasFloat} =
     BLAS.gemm!('N', 'N', α, A, B, β, C)
