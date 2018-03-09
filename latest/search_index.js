3429a2af35a6ef8cf671df864c2cd46bca405fa1
autodocs <autodocs>
2018-03-09 11:08:39 +0000

    build based on 0b9360f

--- latest/search_index.js
+++ latest/search_index.js
@@ -17,6 +17,22 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "index.html#BandedMatrices.BandedMatrix",
+    "page": "Home",
+    "title": "BandedMatrices.BandedMatrix",
+    "category": "type",
+    "text": "BandedMatrix{T}(uninitialized, (n, m), (l, u))\n\nreturns an uninitialized n×m banded matrix of type T with bandwidths (l,u).\n\n\n\n"
+},
+
+{
+    "location": "index.html#BandedMatrices.brand",
+    "page": "Home",
+    "title": "BandedMatrices.brand",
+    "category": "function",
+    "text": "brand(T,n,m,l,u)\n\nCreates an n×m banded matrix  with random numbers in the bandwidth of type T with bandwidths (l,u)\n\n\n\n"
+},
+
+{
     "location": "index.html#Creating-banded-matrices-1",
     "page": "Home",
     "title": "Creating banded matrices",
@@ -25,11 +41,67 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "index.html#BandedMatrices.bandwidths",
+    "page": "Home",
+    "title": "BandedMatrices.bandwidths",
+    "category": "function",
+    "text": "bandwidths(A)\n\nReturns a tuple containing the upper and lower bandwidth of A.\n\n\n\n"
+},
+
+{
+    "location": "index.html#BandedMatrices.bandwidth",
+    "page": "Home",
+    "title": "BandedMatrices.bandwidth",
+    "category": "function",
+    "text": "bandwidth(A,i)\n\nReturns the lower bandwidth (i==1) or the upper bandwidth (i==2).\n\n\n\n"
+},
+
+{
+    "location": "index.html#BandedMatrices.bandrange",
+    "page": "Home",
+    "title": "BandedMatrices.bandrange",
+    "category": "function",
+    "text": "bandrange(A)\n\nReturns the range -bandwidth(A,1):bandwidth(A,2).\n\n\n\n"
+},
+
+{
+    "location": "index.html#BandedMatrices.band",
+    "page": "Home",
+    "title": "BandedMatrices.band",
+    "category": "function",
+    "text": "band(i)\n\nRepresents the i-th band of a banded matrix.\n\njulia> using BandedMatrices\n\njulia> A = bones(5,5,1,1)\n5×5 BandedMatrices.BandedMatrix{Float64}:\n 1.0  1.0\n 1.0  1.0  1.0\n      1.0  1.0  1.0\n           1.0  1.0  1.0\n                1.0  1.0\n\njulia> A[band(1)]\n4-element Array{Float64,1}:\n 1.0\n 1.0\n 1.0\n 1.0\n\n\n\n"
+},
+
+{
+    "location": "index.html#BandedMatrices.BandRange",
+    "page": "Home",
+    "title": "BandedMatrices.BandRange",
+    "category": "type",
+    "text": "BandRange\n\nRepresents the entries in a row/column inside the bands.\n\njulia> using BandedMatrices\n\njulia> A = bones(5,5,1,1)\n5×5 BandedMatrices.BandedMatrix{Float64}:\n 1.0  1.0\n 1.0  1.0  1.0\n      1.0  1.0  1.0\n           1.0  1.0  1.0\n                1.0  1.0\n\njulia> A[2,BandRange]\n3-element Array{Float64,1}:\n 1.0\n 1.0\n 1.0\n\n\n\n"
+},
+
+{
     "location": "index.html#Accessing-banded-matrices-1",
     "page": "Home",
     "title": "Accessing banded matrices",
     "category": "section",
     "text": "bandwidthsbandwidthbandrangebandBandRange"
+},
+
+{
+    "location": "index.html#BandedMatrices.SymBandedMatrix",
+    "page": "Home",
+    "title": "BandedMatrices.SymBandedMatrix",
+    "category": "type",
+    "text": "SymBandedMatrix(T, n, k)\n\nreturns an unitialized n×n symmetric banded matrix of type T with bandwidths (-k,k).\n\n\n\n"
+},
+
+{
+    "location": "index.html#BandedMatrices.sbrand",
+    "page": "Home",
+    "title": "BandedMatrices.sbrand",
+    "category": "function",
+    "text": "sbrand(T,n,k)\n\nCreates an n×n symmetric banded matrix  with random numbers in the bandwidth of type T with bandwidths (k,k)\n\n\n\n"
 },
 
 {
