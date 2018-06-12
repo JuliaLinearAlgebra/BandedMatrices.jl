ba7927d2ba7c142a7f791fa1b9e91f2642b1bac9
Sheehan Olver <solver@mac.com>
2018-06-12 19:44:02 +0100

    add @time to get output

--- test/test_miscs.jl
+++ test/test_miscs.jl
@@ -7,64 +7,66 @@ BandedMatrixWithZero = Union{BandedMatrix{Float64}, UniformScaling}
 Base.zero(::Type{BandedMatrixWithZero}) = 0*I
 
 @testset "misc tests" begin
-   A = BandedMatrix{BandedMatrixWithZero}(undef, 1, 2, 0, 1)
-   A[1,1] = BandedMatrix(Eye(1),(0,1))
-   A[1,2] = BandedMatrix(Zeros(1,2),(0,1))
-   A[1,2][1,1] = -1/3
-   A[1,2][1,2] = 1/3
-   B = BandedMatrix{BandedMatrixWithZero}(undef, 2, 1, 1, 1)
-   B[1,1] = 0.2BandedMatrix(Eye(1),(0,1))
-   B[2,1] = BandedMatrix(Zeros(2,1), (1,0))
-   B[2,1][1,1] = -2/30
-   B[2,1][2,1] = 1/3
+    @time @testset "BandedMatrixWithZero" begin
+        A = BandedMatrix{BandedMatrixWithZero}(undef, 1, 2, 0, 1)
+        A[1,1] = BandedMatrix(Eye(1),(0,1))
+        A[1,2] = BandedMatrix(Zeros(1,2),(0,1))
+        A[1,2][1,1] = -1/3
+        A[1,2][1,2] = 1/3
+        B = BandedMatrix{BandedMatrixWithZero}(undef, 2, 1, 1, 1)
+        B[1,1] = 0.2BandedMatrix(Eye(1),(0,1))
+        B[2,1] = BandedMatrix(Zeros(2,1), (1,0))
+        B[2,1][1,1] = -2/30
+        B[2,1][2,1] = 1/3
 
-   @test (A*B)[1,1][1,1] ≈ 1/3
+        @test (A*B)[1,1][1,1] ≈ 1/3
+    end
 
 
 
-   # Test dense overrides
-   A = rand(10,11)
-   @test bandwidths(A) == (9,10)
-   A = rand(10)
-   @test bandwidths(A) == (9,0)
-   @test bandwidths(A') == (0,9)
-
-   # test trivial convert routines
-
-   A = brand(3,4,1,2)
-   @test isa(BandedMatrix{Float64}(A), BandedMatrix{Float64})
-   @test isa(AbstractMatrix{Float64}(A), BandedMatrix{Float64})
-   @test isa(AbstractArray{Float64}(A), BandedMatrix{Float64})
-   @test isa(BandedMatrix(A), BandedMatrix{Float64})
-   @test isa(AbstractMatrix(A), BandedMatrix{Float64})
-   @test isa(AbstractArray(A), BandedMatrix{Float64})
-   @test isa(BandedMatrix{ComplexF16}(A), BandedMatrix{ComplexF16})
-   @test isa(AbstractMatrix{ComplexF16}(A), BandedMatrix{ComplexF16})
-   @test isa(AbstractArray{ComplexF16}(A), BandedMatrix{ComplexF16})
-
-   # Test show function
-   if VERSION < v"0.7-"
-      @test occursin("10×10 BandedMatrices.BandedMatrix{Float64,Array{Float64,2}}",
-         sprint() do io
-             show(io, MIME"text/plain"(), brand(10, 10, 3, 3))
-         end)
-   else
-      @test occursin("10×10 BandedMatrix{Float64,Array{Float64,2}}",
-         sprint() do io
-            show(io, MIME"text/plain"(), brand(10, 10, 3, 3))
-         end)
-   end
-   if VERSION < v"0.7-"
-      needle = "1.0  0.0     \n 0.0  1.0  0.0\n      0.0  1.0"
-   else
-      needle = "1.0  0.0  0.0\n 0.0  1.0  0.0\n 0.0  0.0  1.0"
-   end
-   @test occursin(needle, sprint() do io
-         show(io, MIME"text/plain"(), BandedMatrix(Eye(3),(1,1)))
-      end)
-
-   # Issue #27
-   A=brand(1,10,0,9)
-   B=brand(10,10,255,255)
-   @test Matrix(A*B)  ≈ Matrix(A)*Matrix(B)
+    @time @testset "dense overrides" begin
+        A = rand(10,11)
+        @test bandwidths(A) == (9,10)
+        A = rand(10)
+        @test bandwidths(A) == (9,0)
+        @test bandwidths(A') == (0,9)
+    end
+    @time @testset "trivial convert routines" begin
+        A = brand(3,4,1,2)
+        @test isa(BandedMatrix{Float64}(A), BandedMatrix{Float64})
+        @test isa(AbstractMatrix{Float64}(A), BandedMatrix{Float64})
+        @test isa(AbstractArray{Float64}(A), BandedMatrix{Float64})
+        @test isa(BandedMatrix(A), BandedMatrix{Float64})
+        @test isa(AbstractMatrix(A), BandedMatrix{Float64})
+        @test isa(AbstractArray(A), BandedMatrix{Float64})
+        @test isa(BandedMatrix{ComplexF16}(A), BandedMatrix{ComplexF16})
+        @test isa(AbstractMatrix{ComplexF16}(A), BandedMatrix{ComplexF16})
+        @test isa(AbstractArray{ComplexF16}(A), BandedMatrix{ComplexF16})
+    end
+    @time @testset "show" begin
+        if VERSION < v"0.7-"
+          @test occursin("10×10 BandedMatrices.BandedMatrix{Float64,Array{Float64,2}}",
+             sprint() do io
+                 show(io, MIME"text/plain"(), brand(10, 10, 3, 3))
+             end)
+        else
+          @test occursin("10×10 BandedMatrix{Float64,Array{Float64,2}}",
+             sprint() do io
+                show(io, MIME"text/plain"(), brand(10, 10, 3, 3))
+             end)
+        end
+        if VERSION < v"0.7-"
+          needle = "1.0  0.0     \n 0.0  1.0  0.0\n      0.0  1.0"
+        else
+          needle = "1.0  0.0  0.0\n 0.0  1.0  0.0\n 0.0  0.0  1.0"
+        end
+        @test occursin(needle, sprint() do io
+             show(io, MIME"text/plain"(), BandedMatrix(Eye(3),(1,1)))
+          end)
+    end
+    @time @testset "Issue #27" begin
+        A=brand(1,10,0,9)
+        B=brand(10,10,255,255)
+        @test Matrix(A*B)  ≈ Matrix(A)*Matrix(B)
+    end
 end
