7d5fd3cf5a156f2cbe97e674418bafa153db0957
Sheehan Olver <solver@mac.com>
2018-09-07 20:28:00 +0100

    Bump docs version

--- docs/make.jl
+++ docs/make.jl
@@ -15,7 +15,7 @@ makedocs(modules=[BandedMatrices],
 deploydocs(
     repo   = "github.com/JuliaMatrices/BandedMatrices.jl.git",
     latest = "master",
-    julia  = "0.6",
+    julia  = "1.0",
     osname = "linux",
     target = "build",
     deps   = nothing,
