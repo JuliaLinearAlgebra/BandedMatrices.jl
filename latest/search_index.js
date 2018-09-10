a6f12cc6d5eaa8eece97e6c5242680cb466da32b
autodocs <autodocs>
2018-09-10 08:55:47 +0000

    build based on 4e2f22f

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "BandedMatrices.BandedMatrix",
     "category": "type",
-    "text": "BandedMatrix{T}(undef, (n, m), (l, u))\n\nreturns an uninitialized n×m banded matrix of type T with bandwidths (l,u).\n\n\n\nBandedMatrix{T}(kv::Pair, (m,n), (l,u))\n\nConstruct a m × n BandedMatrix with bandwidths (l,u) from Pairs of diagonals and vectors. Vector kv.second will be placed on the kv.first diagonal.\n\n\n\nBandedMatrix(kv::Pair{<:Integer,<:AbstractVector}...)\n\nConstruct a square matrix from Pairs of diagonals and vectors. Vector kv.second will be placed on the kv.first diagonal.\n\n\n\n"
+    "text": "BandedMatrix{T}(undef, (n, m), (l, u))\n\nreturns an uninitialized n×m banded matrix of type T with bandwidths (l,u).\n\n\n\n\n\nBandedMatrix{T}(kv::Pair, (m,n), (l,u))\n\nConstruct a m × n BandedMatrix with bandwidths (l,u) from Pairs of diagonals and vectors. Vector kv.second will be placed on the kv.first diagonal.\n\n\n\n\n\nBandedMatrix(kv::Pair{<:Integer,<:AbstractVector}...)\n\nConstruct a square matrix from Pairs of diagonals and vectors. Vector kv.second will be placed on the kv.first diagonal.\n\n\n\n\n\n"
 },
 
 {
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "BandedMatrices.brand",
     "category": "function",
-    "text": "brand(T,n,m,l,u)\n\nCreates an n×m banded matrix  with random numbers in the bandwidth of type T with bandwidths (l,u)\n\n\n\n"
+    "text": "brand(T,n,m,l,u)\n\nCreates an n×m banded matrix  with random numbers in the bandwidth of type T with bandwidths (l,u)\n\n\n\n\n\n"
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Creating banded matrices",
     "category": "section",
-    "text": "BandedMatrixbonesbrandTo create a banded matrix of all zeros, identity matrix, or with a constant value use the following constructors:julia> BandedMatrix(Zeros(5,5), (1,2))\n5×5 BandedMatrices.BandedMatrix{Float64,Array{Float64,2}}:\n 0.0  0.0  0.0          \n 0.0  0.0  0.0  0.0     \n      0.0  0.0  0.0  0.0\n           0.0  0.0  0.0\n                0.0  0.0\n\njulia> BandedMatrix(Eye(5), (1,2))\n5×5 BandedMatrices.BandedMatrix{Float64,Array{Float64,2}}:\n 1.0  0.0  0.0          \n 0.0  1.0  0.0  0.0     \n      0.0  1.0  0.0  0.0\n           0.0  1.0  0.0\n                0.0  1.0\n\njulia> BandedMatrix(Ones(5,5), (1,2))\n5×5 BandedMatrices.BandedMatrix{Float64,Array{Float64,2}}:\n 1.0  1.0  1.0          \n 1.0  1.0  1.0  1.0     \n      1.0  1.0  1.0  1.0\n           1.0  1.0  1.0\n                1.0  1.0To create a banded matrix of a given size with constant bands (such as the classical finite difference approximation of the one-dimensional Laplacian on the unit interval [0,1]), you can use the following:n = 128\nh = 1/n\nA = BandedMatrix{Float64}(undef, (n,n), (1,1))\nA[band(0)] .= -2/h^2\nA[band(1)] .= A[band(-1)] .= 1/h^2"
+    "text": "BandedMatrixbonesbrandTo create a banded matrix of all zeros, identity matrix, or with a constant value use the following constructors:julia> BandedMatrix(Zeros(5,5), (1,2))\n5×5 BandedMatrix{Float64,Array{Float64,2}}:\n 0.0  0.0  0.0   ⋅    ⋅\n 0.0  0.0  0.0  0.0   ⋅\n  ⋅   0.0  0.0  0.0  0.0\n  ⋅    ⋅   0.0  0.0  0.0\n  ⋅    ⋅    ⋅   0.0  0.0\n\njulia> BandedMatrix(Eye(5), (1,2))\n5×5 BandedMatrix{Float64,Array{Float64,2}}:\n 1.0  0.0  0.0   ⋅    ⋅\n 0.0  1.0  0.0  0.0   ⋅\n  ⋅   0.0  1.0  0.0  0.0\n  ⋅    ⋅   0.0  1.0  0.0\n  ⋅    ⋅    ⋅   0.0  1.0\n\njulia> BandedMatrix(Ones(5,5), (1,2))\n5×5 BandedMatrix{Float64,Array{Float64,2}}:\n 1.0  1.0  1.0   ⋅    ⋅\n 1.0  1.0  1.0  1.0   ⋅\n  ⋅   1.0  1.0  1.0  1.0\n  ⋅    ⋅   1.0  1.0  1.0\n  ⋅    ⋅    ⋅   1.0  1.0To create a banded matrix of a given size with constant bands (such as the classical finite difference approximation of the one-dimensional Laplacian on the unit interval [0,1]), you can use the following:n = 128\nh = 1/n\nA = BandedMatrix{Float64}(undef, (n,n), (1,1))\nA[band(0)] .= -2/h^2\nA[band(1)] .= A[band(-1)] .= 1/h^2"
 },
 
 {
@@ -45,7 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "BandedMatrices.bandwidths",
     "category": "function",
-    "text": "bandwidths(A)\n\nReturns a tuple containing the upper and lower bandwidth of A.\n\n\n\n"
+    "text": "bandwidths(A)\n\nReturns a tuple containing the upper and lower bandwidth of A.\n\n\n\n\n\n"
 },
 
 {
@@ -53,7 +53,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "BandedMatrices.bandwidth",
     "category": "function",
-    "text": "bandwidth(A,i)\n\nReturns the lower bandwidth (i==1) or the upper bandwidth (i==2).\n\n\n\n"
+    "text": "bandwidth(A,i)\n\nReturns the lower bandwidth (i==1) or the upper bandwidth (i==2).\n\n\n\n\n\n"
 },
 
 {
@@ -61,7 +61,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "BandedMatrices.bandrange",
     "category": "function",
-    "text": "bandrange(A)\n\nReturns the range -bandwidth(A,1):bandwidth(A,2).\n\n\n\n"
+    "text": "bandrange(A)\n\nReturns the range -bandwidth(A,1):bandwidth(A,2).\n\n\n\n\n\n"
 },
 
 {
@@ -69,15 +69,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "BandedMatrices.band",
     "category": "function",
-    "text": "band(i)\n\nRepresents the i-th band of a banded matrix.\n\njulia> using BandedMatrices\n\njulia> A = bones(5,5,1,1)\n5×5 BandedMatrices.BandedMatrix{Float64}:\n 1.0  1.0\n 1.0  1.0  1.0\n      1.0  1.0  1.0\n           1.0  1.0  1.0\n                1.0  1.0\n\njulia> A[band(1)]\n4-element Array{Float64,1}:\n 1.0\n 1.0\n 1.0\n 1.0\n\n\n\n"
-},
-
-{
-    "location": "index.html#BandedMatrices.BandRange",
-    "page": "Home",
-    "title": "BandedMatrices.BandRange",
-    "category": "type",
-    "text": "BandRange\n\nRepresents the entries in a row/column inside the bands.\n\njulia> using BandedMatrices\n\njulia> A = bones(5,5,1,1)\n5×5 BandedMatrices.BandedMatrix{Float64}:\n 1.0  1.0\n 1.0  1.0  1.0\n      1.0  1.0  1.0\n           1.0  1.0  1.0\n                1.0  1.0\n\njulia> A[2,BandRange]\n3-element Array{Float64,1}:\n 1.0\n 1.0\n 1.0\n\n\n\n"
+    "text": "band(i)\n\nRepresents the i-th band of a banded matrix.\n\njulia> using BandedMatrices\n\njulia> A = bones(5,5,1,1)\n5×5 BandedMatrices.BandedMatrix{Float64}:\n 1.0  1.0\n 1.0  1.0  1.0\n      1.0  1.0  1.0\n           1.0  1.0  1.0\n                1.0  1.0\n\njulia> A[band(1)]\n4-element Array{Float64,1}:\n 1.0\n 1.0\n 1.0\n 1.0\n\n\n\n\n\n"
 },
 
 {
@@ -89,27 +81,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#BandedMatrices.SymBandedMatrix",
-    "page": "Home",
-    "title": "BandedMatrices.SymBandedMatrix",
-    "category": "type",
-    "text": "SymBandedMatrix(T, n, k)\n\nreturns an unitialized n×n symmetric banded matrix of type T with bandwidths (-k,k).\n\n\n\n"
-},
-
-{
-    "location": "index.html#BandedMatrices.sbrand",
-    "page": "Home",
-    "title": "BandedMatrices.sbrand",
-    "category": "function",
-    "text": "sbrand(T,n,k)\n\nCreates an n×n symmetric banded matrix  with random numbers in the bandwidth of type T with bandwidths (k,k)\n\n\n\n"
-},
-
-{
     "location": "index.html#Creating-symmetric-banded-matrices-1",
     "page": "Home",
     "title": "Creating symmetric banded matrices",
     "category": "section",
-    "text": "SymBandedMatrixsbonessbrand"
+    "text": "Use Symmetric(::BandedMatrix) to work with symmetric banded matrices."
 },
 
 {
@@ -117,7 +93,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Banded matrix interface",
     "category": "section",
-    "text": "Banded matrices go beyond the type BandedMatrix: one can also create matrix types that conform to the _banded matrix interface_, in which case many of the utility functions in this package are available. The banded matrix interface consists of the following:Required methods Brief description\nbandwidth(A, k) Returns the sub-diagonal bandwidth (k==1) or the super-diagonal bandwidth (k==2)\nisbanded(A) Override to return true\ninbands_getindex(A, k, j) Unsafe: return A[k,j], without the need to check if we are inside the bands\ninbands_setindex!(A, v, k, j) Unsafe: set A[k,j] = v, without the need to check if we are inside the bandsNote that certain SubArrays of BandedMatrix are also banded matrices. The banded matrix interface is implemented for such SubArrays to take advantage of this."
+    "text": "Banded matrices go beyond the type BandedMatrix: one can also create matrix types that conform to the banded matrix interface, in which case many of the utility functions in this package are available. The banded matrix interface consists of the following:Required methods Brief description\nbandwidths(A) Returns a tuple containing the sub-diagonal and super-diagonal bandwidth\nisbanded(A) Override to return true\n–––––––- –––––––-\nOptional methods Brief description\n–––––––- –––––––-\ninbands_getindex(A, k, j) Unsafe: return A[k,j], without the need to check if we are inside the bands\ninbands_setindex!(A, v, k, j) Unsafe: set A[k,j] = v, without the need to check if we are inside the bands\nBandedMatrices.MemoryLayout(A) Override to get banded lazy linear algebra, e.g. y .= Mul(A,x)\nBandedMatrices.bandeddata(A) Override to return a matrix of the entries in BLAS format. Required if MemoryLayout(A) returns BandedColumnMajorNote that certain SubArrays of BandedMatrix are also banded matrices. The banded matrix interface is implemented for such SubArrays to take advantage of this."
 },
 
 {
@@ -125,7 +101,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Implementation",
     "category": "section",
-    "text": "Currently, only column-major ordering is supported: a banded matrix B[ a_11 a_12\n  a_21 a_22 a_23\n  a_31 a_32 a_33 a_34\n       a_42 a_43 a_44  ]is represented as a BandedMatrix with a field B.data representing the matrix as[ *     a_12   a_23    a_34\n a_11   a_22   a_33    a_44\n a_21   a_32   a_43    *\n a_31   a_42   *       *       ]B.l gives the number of subdiagonals (2) and B.u gives the number of super-diagonals (1).  Both B.l and B.u must be non-negative at the moment."
+    "text": "Currently, only column-major ordering is supported: a banded matrix B[ a_11 a_12  ⋅    ⋅\n  a_21 a_22 a_23  ⋅\n  a_31 a_32 a_33 a_34\n   ⋅   a_42 a_43 a_44  ]is represented as a BandedMatrix with a field B.data representing the matrix as[ ⋅     a_12   a_23    a_34\n a_11   a_22   a_33    a_44\n a_21   a_32   a_43    ⋅\n a_31   a_42   ⋅       ⋅       ]B.l gives the number of subdiagonals (2) and B.u gives the number of super-diagonals (1).  Both B.l and B.u must be non-negative at the moment."
 },
 
 ]}
