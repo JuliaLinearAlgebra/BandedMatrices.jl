2ce86fe41e2daa9f0cfff6b2c2444ce1cec67da8
Sheehan Olver <solver@mac.com>
2019-03-11 16:44:32 +0000

    add error tests

--- test/test_bandedqr.jl
+++ test/test_bandedqr.jl
@@ -32,18 +32,36 @@ using BandedMatrices, LinearAlgebra, Test
         @test qr(A).τ ≈ LinearAlgebra.qrfactUnblocked!(Matrix(A)).τ
         b=rand(T,100)
         @test qr(A)\b ≈ Matrix(A)\b
+        b=rand(T,100,2)
+        @test qr(A)\b ≈ Matrix(A)\b
+        @test_throws DimensionMismatch qr(A) \ randn(3)
+        @test_throws DimensionMismatch qr(A).Q'randn(3)
 
         A=brand(T,102,100,3,4)
         @test qr(A).factors ≈ LinearAlgebra.qrfactUnblocked!(Matrix(A)).factors
         @test qr(A).τ ≈ LinearAlgebra.qrfactUnblocked!(Matrix(A)).τ
         b=rand(T,102)
         @test qr(A)\b ≈ Matrix(A)\b
+        b=rand(T,102,2)
+        @test qr(A)\b ≈ Matrix(A)\b
+        @test_throws DimensionMismatch qr(A) \ randn(3)
+        @test_throws DimensionMismatch qr(A).Q'randn(3)
 
         A=brand(T,100,102,3,4)
         @test qr(A).factors ≈ LinearAlgebra.qrfactUnblocked!(Matrix(A)).factors
         @test qr(A).τ ≈ LinearAlgebra.qrfactUnblocked!(Matrix(A)).τ
         b=rand(T,100)
         @test_broken qr(A)\b ≈ Matrix(A)\b
+
+        A = Tridiagonal(randn(T,99), randn(T,100), randn(T,99))
+        @test qr(A).factors ≈ LinearAlgebra.qrfactUnblocked!(Matrix(A)).factors
+        @test qr(A).τ ≈ LinearAlgebra.qrfactUnblocked!(Matrix(A)).τ
+        b=rand(T,100)
+        @test qr(A)\b ≈ Matrix(A)\b
+        b=rand(T,100,2)
+        @test qr(A)\b ≈ Matrix(A)\b
+        @test_throws DimensionMismatch qr(A) \ randn(3)
+        @test_throws DimensionMismatch qr(A).Q'randn(3)
     end
 
     @testset "Mixed types" begin
