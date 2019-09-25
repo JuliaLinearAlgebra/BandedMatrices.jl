da952116a0cbd16918acca2abe6bb5aea1b22697
autodocs <autodocs>
2019-09-25 15:55:44 +0000

    build based on c21fd42

--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.12",
   "release-0.11",
   "release-0.10",
   "release-0.9",
@@ -11,6 +12,7 @@ var DOC_VERSIONS = [
   "release-0.4",
   "release-0.3",
   "release-0.2",
+  "v0.12.0",
   "v0.11.1",
   "v0.11.0",
   "v0.10.1",
