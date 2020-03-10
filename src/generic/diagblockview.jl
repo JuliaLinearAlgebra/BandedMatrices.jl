a7828cea8c63e9bac3a02fa6441c4900da84b101
Sheehan Olver <solver@mac.com>
2020-03-10 16:50:31 +0000

    LAPACK tpqrt2 call

--- src/generic/diagblockview.jl
+++ src/generic/diagblockview.jl
@@ -1,4 +1,71 @@
 using BandedMatrices, BlockBandedMatrices, BlockArrays
+
+import LinearAlgebra.BLAS: @blasfunc, BlasInt, require_one_based_indexing, chkstride1
+import LinearAlgebra.LAPACK: chklapackerror, liblapack
+
+
+
+for (tpqrt,elty) in ((:dtpqrt2_,:Float64),)
+    @eval function tpqrt2!(l_in::Integer, A::AbstractMatrix{$elty}, B::AbstractMatrix{$elty}, T::AbstractMatrix{$elty})
+        require_one_based_indexing(A, B, T)
+        chkstride1(A,B,T)
+        m     = BlasInt(size(B, 1))
+        n     = BlasInt(size(B, 2))
+        l = BlasInt(l_in)
+        if !(min(m,n) ≥ l ≥ 0)
+            throw(DimensionMismatch("too many $l"))
+        end
+        if size(A) ≠ (n,n)
+            throw(DimensionMismatch("A has size $(size(A)), but needs size ($n,$n)"))
+        end
+        if size(T) ≠ (n,n)
+            throw(DimensionMismatch("T has size $(size(T)), but needs size ($n,$n)"))
+        end
+        lda   = BlasInt(max(1,stride(A, 2)))
+        ldb   = BlasInt(max(1,stride(B, 2)))
+        ldt = BlasInt(max(1,stride(T,2)))
+
+        info  = Ref{BlasInt}()
+        ccall((@blasfunc($tpqrt), liblapack), Cvoid,
+                (Ref{BlasInt}, Ref{BlasInt}, Ref{BlasInt}, 
+                Ptr{$elty}, Ref{BlasInt},
+                Ptr{$elty}, Ref{BlasInt},
+                Ptr{$elty}, Ref{BlasInt},
+                Ptr{BlasInt}),
+                m, n, l, A, lda, B, ldb, T, ldt, info)
+        chklapackerror(info[])
+        A, B, T
+    end
+end
+
+A = randn(n,n)
+B = randn(n,n)
+T = similar(A,n,n)
+
+A2,B2,T2 = tpqrt2!(2,copy(A), copy(B), copy(T))
+
+qr([UpperTriangular(A); UpperTriangular(B)]).T
+
+A2
+B2
+
+T2
+
+
+
+F = LinearAlgebra.qrfactUnblocked!([A; B])
+F.factors
+F.τ
+
+FA = LinearAlgebra.qrfactUnblocked!(copy(A))
+
+FA.Q'*A
+
+B
+
+FA.factors
+
+# DTPQRT2
 import BandedMatrices: bandeddata
 import Base: to_indices, iterate, length, getindex, view, unsafe_view, SubArray, @_inline_meta, viewindexing, ensure_indexable, index_dimsum, reindex, strides, unsafe_convert
 import BlockArrays: BlockSlice
