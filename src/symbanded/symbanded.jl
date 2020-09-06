eb5a13a44e6c2d8500532dc0fd9d64f214e97caf
Sheehan Olver <solver@mac.com>
2020-09-06 12:16:46 +0100

    Support bandwidth with a nested Broadcast (#201)
    * Support bandwidth with a nested Broadcast
    
    * Special Symmetric getindex
    
    * Update symbanded.jl
    
    * fixes for julia 1.0

--- src/symbanded/symbanded.jl
+++ src/symbanded/symbanded.jl
@@ -16,7 +16,21 @@
 @inline inbands_getindex(A::Symmetric{<:Any, <:BandedMatrix}, k::Integer, j::Integer) =
     parent(A).data[bandwidth(A) - abs(k-j) + 1, max(k,j)]
 
-
+# this is a hack but is much faster than default
+if VERSION ≥ v"1.5"
+    function getindex(S::Symmetric{<:Any, <:BandedMatrix}, kr::AbstractUnitRange, jr::AbstractUnitRange)
+        A = parent(S)
+        m = max(maximum(kr), maximum(jr))
+        B = if S.uplo == 'U'
+            BandedMatrix(A[1:m,1:m],(0, bandwidth(A,2)))
+        else
+            BandedMatrix(A[1:m,1:m],(bandwidth(A,1),0))
+        end
+        ret = B + transpose(B)
+        rdiv!(view(ret, band(0)), 2)
+        ret[kr, jr]
+    end
+end
 
 symmetriclayout(::ML) where ML<:BandedColumns = SymmetricLayout{ML}()
 symmetriclayout(::ML) where ML<:BandedRows = SymmetricLayout{ML}()
--- test/test_broadcasting.jl
+++ test/test_broadcasting.jl
@@ -524,4 +524,12 @@ import BandedMatrices: BandedStyle, BandedRows
         C .= A .+ B
         @test diag(C) == Zeros{Int}(10)
     end
+
+    @testset "nested broadcast" begin
+        A = brand(5,4,2,1)
+        x = randn(5)
+        B = Base.Broadcast.broadcasted(*, Base.Broadcast.broadcasted(+, 2, x), A)
+        @test bandwidths(B) == bandwidths(A) == bandwidths(materialize(B)) == (2,1)
+        @test materialize(B) == (2 .+ x) .* Matrix(A)
+    end
 end
