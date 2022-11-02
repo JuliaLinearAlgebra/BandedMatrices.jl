2be91226efe066e0e5ea123635c87bd9e96319f0
Daniel Karrasch <daniel.karrasch@posteo.de>
2022-11-02 22:55:29 +0100

    Make `cholesky` work on v1.9 (#276)
    * Make `cholesky` work on v1.9
    
    * fix ambiguity
    
    * fix version

--- src/symbanded/BandedCholesky.jl
+++ src/symbanded/BandedCholesky.jl
@@ -93,8 +93,10 @@ ldiv!(A::Cholesky{T,<:AbstractBandedMatrix}, B::StridedVecOrMat{T}) where T<:Bla
 
 
 # For some bizarre reason this isnt in LinearAlgebra
-ArrayLayouts._cholesky(::Union{SymmetricLayout{<:AbstractBandedLayout},HermitianLayout{<:AbstractBandedLayout}}, ax, A, ::Val{false}=Val(false); check::Bool = true) = cholesky!(cholcopy(A); check = check)
+ArrayLayouts._cholesky(::Union{SymmetricLayout{<:AbstractBandedLayout},HermitianLayout{<:AbstractBandedLayout}}, ax, A, ::Val{false}=Val(false); check::Bool = true) =
+    cholesky!(cholcopy(A); check = check)
 
-
+cholcopy(A::RealHermSymComplexHerm{<:Any,<:BandedMatrix}) =
+    copyto!(similar(A, LinearAlgebra.choltype(A)), A)
 # cholesky(A::Hermitian{T,<:BandedMatrix{T}},
 #     ::Val{false}=Val(false); check::Bool = true) where T = cholesky!(cholcopy(A); check = check)
