3bc3c03534c036fe7d937269614e1fd0b1267beb
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-02-15 12:48:22 +0530

    skip zerobands check for higher dest bandwidth

--- src/generic/broadcast.jl
+++ src/generic/broadcast.jl
@@ -77,7 +77,7 @@ function checkzerobands(dest, f, A::AbstractMatrix)
     d_l, d_u = bandwidths(dest)
     l, u = bandwidths(A)
 
-    if (l,u) ≠ (d_l,d_u)
+    if !(d_l >= l && d_u >= u)
         for j = rowsupport(A)
             for k = max(1,j-u) : min(j-d_u-1,m)
                 iszero(f(A[k,j])) || throw(BandError(dest,j-k))
