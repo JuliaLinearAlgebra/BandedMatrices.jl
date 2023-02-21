2ad7d258c1ee2a3a27e49090ef634fb979c7836d
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-02-21 14:46:04 +0400

    improve band doctest examples

--- src/generic/Band.jl
+++ src/generic/Band.jl
@@ -13,20 +13,31 @@ Represents the `i`-th band of a banded matrix.
 ```jldoctest
 julia> using BandedMatrices
 
-julia> A = BandedMatrix(Ones(5,5),(1,1))
-5×5 BandedMatrix{Float64} with bandwidths (1, 1):
- 1.0  1.0   ⋅    ⋅    ⋅
- 1.0  1.0  1.0   ⋅    ⋅
-  ⋅   1.0  1.0  1.0   ⋅
-  ⋅    ⋅   1.0  1.0  1.0
-  ⋅    ⋅    ⋅   1.0  1.0
+julia> A = BandedMatrix(0=>1:4, 1=>5:7, -1=>8:10)
+4×4 BandedMatrix{Int64} with bandwidths (1, 1):
+ 1  5   ⋅  ⋅
+ 8  2   6  ⋅
+ ⋅  9   3  7
+ ⋅  ⋅  10  4
 
 julia> A[band(1)]
-4-element Vector{Float64}:
- 1.0
- 1.0
- 1.0
- 1.0
+3-element Vector{Int64}:
+ 5
+ 6
+ 7
+
+julia> A[band(0)]
+4-element Vector{Int64}:
+ 1
+ 2
+ 3
+ 4
+
+julia> A[band(-1)]
+3-element Vector{Int64}:
+  8
+  9
+ 10
 ```
 """
 band(i::Int) = Band(i)
@@ -41,19 +52,18 @@ Represents the entries in a row/column inside the bands.
 ```jldoctest
 julia> using BandedMatrices
 
-julia> A = BandedMatrix(Ones(5,5),(1,1))
-5×5 BandedMatrix{Float64} with bandwidths (1, 1):
- 1.0  1.0   ⋅    ⋅    ⋅
- 1.0  1.0  1.0   ⋅    ⋅
-  ⋅   1.0  1.0  1.0   ⋅
-  ⋅    ⋅   1.0  1.0  1.0
-  ⋅    ⋅    ⋅   1.0  1.0
+julia> A = BandedMatrix(0=>1:4, 1=>5:7, -1=>8:10)
+4×4 BandedMatrix{Int64} with bandwidths (1, 1):
+ 1  5   ⋅  ⋅
+ 8  2   6  ⋅
+ ⋅  9   3  7
+ ⋅  ⋅  10  4
 
-julia> A[2,BandRange]
-3-element Vector{Float64}:
- 1.0
- 1.0
- 1.0
+julia> A[2, BandRange]
+3-element Vector{Int64}:
+ 8
+ 2
+ 6
 ```
 """
 const BandRange = BandRangeType()
