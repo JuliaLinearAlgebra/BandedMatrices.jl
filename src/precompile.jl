7988daff4a4099f5d3d7c6447552938bbaf38529
Tim Holy <tim.holy@gmail.com>
2023-04-25 04:31:47 -0500

    Migrate from SnoopPrecompile to PrecompileTools (#347)

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,9 +1,9 @@
-using SnoopPrecompile
+using PrecompileTools
 
-@precompile_setup begin
+@setup_workload begin
 	vs = ([1.0], Float32[1.0], ComplexF32[1.0], ComplexF64[1.0])
 	Bs = Any[BandedMatrix(0 => v) for v in vs]
-	@precompile_all_calls begin
+	@compile_workload begin
 		for B in Bs, op in (+, -, *)
 			op(B, B)
 		end
