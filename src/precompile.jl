42c498f051a6bb4f2e4980d3e879c0d970ecd097
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-07-21 10:11:44 +0530

    don't precompile for Float32/ComplexF32 (#381)
    * don't precompile for Float32/ComplexF32
    
    * bump version to v0.17.32

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,7 @@
 using PrecompileTools
 
 @setup_workload begin
-	vs = ([1.0], Float32[1.0], ComplexF32[1.0], ComplexF64[1.0])
+	vs = ([1.0], ComplexF64[1.0])
 	Bs = Any[BandedMatrix(0 => v) for v in vs]
 	@compile_workload begin
 		for B in Bs, op in (+, -, *)
