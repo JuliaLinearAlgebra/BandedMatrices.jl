cfc895e39ce5e5854d00a9b707b0ae5c8ccb6798
Maxim Vassiliev <vassiliev.max@gmail.com>
2024-07-15 15:38:15 +0100

    revert tests to \approx

--- test/test_sum.jl
+++ test/test_sum.jl
@@ -10,9 +10,9 @@ matr = Matrix(r)
     @test sum(empty_r) == 0
     @test sum(empty_r; dims = 2) == zeros(n,1)
     @test sum(empty_r; dims = 1) == zeros(1,m)
-    @test sum(r) == sum(matr)
-    @test sum(r; dims=2) == sum(matr; dims=2)
-    @test sum(r; dims=1) == sum(matr; dims=1)
+    @test sum(r) ≈ sum(matr) rtol = 1e-10
+    @test sum(r; dims=2) ≈ sum(matr; dims=2) rtol = 1e-10
+    @test sum(r; dims=1) ≈ sum(matr; dims=1) rtol = 1e-10
     @test sum(r; dims=3) == r
     @test_throws ArgumentError sum(r; dims=0)
 end
