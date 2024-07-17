e31183fb865953f73dff2b18ebb6ef4924a26e77
maximvassiliev <vassiliev.max@gmail.com>
2024-07-17 20:56:37 +0100

    add unit tests

--- ext/BandedMatricesSparseArraysExt.jl
+++ ext/BandedMatricesSparseArraysExt.jl
@@ -11,13 +11,14 @@ end
 
 function BandedMatrices.bandwidths(A::SparseMatrixCSC)
     l = u = -max(size(A,1),size(A,2))
-
     n = size(A)[2]
     rows = rowvals(A)
     vals = nonzeros(A)
+
     if isempty(vals)
         return bandwidths(Zeros(1))
     end
+
     for j = 1:n
         for ind in nzrange(A, j)
             i = rows[ind]
@@ -32,20 +33,21 @@ function BandedMatrices.bandwidths(A::SparseMatrixCSC)
     l,u
 end
 
+#Treat as n x 1 matrix
 function BandedMatrices.bandwidths(A::SparseVector)
     l = u = -size(A,1)
-
     rows = rowvals(A)
+
     if isempty(rows)
         return bandwidths(Zeros(1))
     end
+
     for i in rows
-        # We skip non-structural zeros when computing the
-        # bandwidths.
         iszero(i) && continue
         u = max(u, 1-i)
         l = max(l, i-1)
     end
+    
     l,u
 end
 
