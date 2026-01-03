9152b256b9f535717ef07c0a806b3d77aea6f01b
Jishnu Bhattacharya <jishnub.github@gmail.com>
2026-01-03 21:31:43 +0400

    Narrow Aqua piracies test

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -3,9 +3,14 @@ module AquaTest
 import BandedMatrices
 import Aqua
 using Test
+using ArrayLayouts: AbstractBandedLayout, checkdimensions, DiagonalLayout,
+                    BidiagonalLayout, TriangularLayout, AbstractTridiagonalLayout
 
 @testset "Project quality" begin
-    Aqua.test_all(BandedMatrices, ambiguities=false, piracies=false)
+    Aqua.test_all(BandedMatrices, ambiguities=false,
+        piracies=(; treat_as_own=Union{Function, Type}[AbstractBandedLayout, checkdimensions, DiagonalLayout,
+                    BidiagonalLayout, TriangularLayout, AbstractTridiagonalLayout], broken=true)
+    )
 end
 
 end
